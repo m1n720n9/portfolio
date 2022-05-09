8c749dc478b27a9213e8e7bff2c3056aa68ac76a
m1n720n9 <skmnjng@gmail.com>
2022-05-09 13:01:16 +0900

    220509

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import './style/Profile.scss';
 import logo from './img/logo.svg';
-import selfie from "./img/zev.jpg";
+import selfie from "./img/pic.jpg";
 
 const Selfie = () => {
   return(
@@ -27,7 +27,7 @@ const Profile = () => {
               <p>NAME 석민정</p>
               <p>BIRTH 1997. 01 .02</p>
               <p>EDUCATION 순천향대학교 신문방송학과</p>
-              <p className="intro">아 졸려요 피곤해요 커피는 맛있는데 잠이 안 깨요...... 바닐라 딜라이트 짱!</p>
+              <p className="intro">~ 한 줄 소개란입니다. 멘트는 신중하고 신중하게 고민 중입니다. ~</p>
             </section>
             <section className="skills">
               <h3>MY SKILLS</h3>
